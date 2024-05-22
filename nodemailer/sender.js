bc75fc5be789bedd59e9f93174dc37fab006a9fc
Alexandru Mihai <115073416+AlexMihai1126@users.noreply.github.com>
2024-05-22 21:35:38 +0300

    cleanup sender.js

--- nodemailer/sender.js
+++ nodemailer/sender.js
@@ -1,13 +1,13 @@
 const transporter = require('./nodemailer');
 require('dotenv').config();
 
-async function sendConfirmationEmail(email,token) {
+async function sendConfirmationEmail(email, token) {
     try {
         const confirmationLink = `http://localhost:${process.env.APP_PORT}/user/confirm?token=${token}`;
 
         await transporter.sendMail({
-            from: `${process.env.EMAIL}`, // Sender email address
-            to: email, // Recipient email address
+            from: `${process.env.EMAIL}`,
+            to: email,
             subject: 'Confirmation Email',
             text: 'Please confirm your email address by clicking the link below:',
             html: `<p>Please confirm your email address by clicking the link below:</p><a href="${confirmationLink}">Click here to confirm your account!</a>`
