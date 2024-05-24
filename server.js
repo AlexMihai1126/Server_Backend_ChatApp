3c043bef65312bc67ff926fb8de287cbdc02aefd
CiurtinAndrei <149907852+CiurtinAndrei@users.noreply.github.com>
2024-05-24 23:29:59 +0300

    Fixed CORS preflight

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ const app = express();
 app.use(cors({
   origin: `*`,
   methods: ['GET', 'POST','DELETE'],
-  allowedHeaders:['sessionId', 'Content-Type'],
+  allowedHeaders:['sessionId', 'Content-Type', 'Authorization'],
   exposedHeaders: ['sessionId'],
 }));
-app.options('*', cors());
+app.options('*', cors({origin:'*'}));
 app.use(express.json({ limit: '50mb' })); // Increase the JSON payload limit
 app.use(express.urlencoded({ extended: true, limit: '50mb' })); // Increase the URL-encoded payload limit
 
